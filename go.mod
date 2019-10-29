d0f8a6251be4f8ee05ce57aff5c2f2430f6f7ea3
Xavier Sala <utrescu@gmail.com>
2019-10-29 14:41:35 +0100

    Connexió amb SQL Server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/utrescu/sqlserverwait
 
 go 1.12
+
+require github.com/denisenkom/go-mssqldb v0.0.0-20191001013358-cfbb681360f0
--- main.go
+++ main.go
@@ -1,27 +1,68 @@
 package main
 
 import (
+	"database/sql"
 	"errors"
 	"fmt"
+	"net/url"
 	"time"
+
+	_ "github.com/denisenkom/go-mssqldb"
+)
+
+var (
+	server   = "localhost"
+	user     = "sa"
+	port     = 1433
+	password = "X1nGuXunG1"
+	database = "BoIsBo"
 )
 
 func connectWithSQLServer() error {
-	return errors.New("Connection failed")
+
+	query := url.Values{}
+	query.Add("database", database)
+
+	u := &url.URL{
+		Scheme:   "sqlserver",
+		User:     url.UserPassword(user, password),
+		Host:     fmt.Sprintf("%s:%d", server, port),
+		RawQuery: query.Encode(),
+	}
+	fmt.Println(u.String())
+	// connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s", server, user, password, port, database)
+	db, err := sql.Open("sqlserver", u.String())
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		return err
+	}
+
+	row := db.QueryRow("SELECT DATABASEPROPERTYEX('BoIsBo', 'Collation') AS Collation")
+	var collation string
+	err = row.Scan(&collation)
+	if err != nil {
+		return err
+	}
+	fmt.Println(collation)
+	return nil
 }
 
 // doItOrFail
-func doItOrFail(timeout <-chan time.Time) error {
+func doItOrFail(timeout <-chan time.Time) (bool, error) {
 
 	tick := time.Tick(500 * time.Millisecond)
 	for {
 		select {
 		case <-timeout:
-			return errors.New("timed out")
+			return false, errors.New("timed out")
 		case <-tick:
 			err := connectWithSQLServer()
 			if err == nil {
-				return nil
+				return true, nil
 			}
 			fmt.Println(err.Error())
 		}
@@ -30,10 +71,12 @@ func doItOrFail(timeout <-chan time.Time) error {
 
 func main() {
 	//
-	timeout := time.After(5 * time.Second)
-	err := doItOrFail(timeout)
+	timeout := time.After(30 * time.Second)
+	ok, err := doItOrFail(timeout)
 	if err != nil {
 		fmt.Printf("ERROR: %s", err.Error())
 	}
+
+	fmt.Printf("Connection: %v", ok)
 
 }
